ecc21da2a20c6e0c79c84d4ba55f180a573c2ea7
Clem <clement.bolin@epitech.eu>
2021-10-26 18:04:02 +0200

    add(api/tools): subreddit about route

--- api/api/router.go
+++ api/api/router.go
@@ -18,6 +18,7 @@ func SetupRouter() *gin.Engine {
 	router.Use(AuthMiddleware)
 	router.GET("/get/me", aboutMe)
 	router.GET("/get/suberredit/list", getListSubreddit)
+	router.GET("/get/suberredit/about", getSuberedditAbout)
 
 	return router
 }
--- api/api/subreddit.go
+++ api/api/subreddit.go
@@ -33,3 +33,30 @@ func getListSubreddit(c *gin.Context) {
 	resp, _ := request.sendRequest()
 	c.JSON(200, resp)
 }
+
+func getSuberedditAbout(c *gin.Context) {
+	var body map[string]interface{}
+	var bodyClient map[string]interface{}
+
+	jsonData, _ := ioutil.ReadAll(c.Request.Body)
+	json.Unmarshal(jsonData, &bodyClient)
+
+	token := bodyClient["token"].(string)
+	subreddit := bodyClient["subreddit"].(string)
+
+	var request Request
+	err := request.createRequest(
+		"https://oauth.reddit.com/" + subreddit + "/about",
+		"GET",
+		body,
+		token,
+	)
+	if err != nil {
+		c.JSON(401, &ErrorResponse{
+			ErrorMessage: err.Error(),
+			Code:         403,
+		})
+	}
+	resp, _ := request.sendRequest()
+	c.JSON(200, resp)
+}
